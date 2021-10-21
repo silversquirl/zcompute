5e3166df3ac19564dfb8aea595fef2044656bd55
Silver <silver@silv.re>
2021-10-21 18:50:58 +0100

    Add vulkan device initialization

--- zcompute.zig
+++ zcompute.zig
@@ -16,6 +16,8 @@ pub const Context = struct {
     phys_device: vk.PhysicalDevice,
     device: vk.Device,
 
+    queue: vk.Queue,
+
     /// BEWARE: Using a GPA with nonzero stack_trace_frames may cause random segmentation faults
     pub fn init(allocator: *std.mem.Allocator) !Context {
         var self: Context = undefined;
@@ -24,12 +26,18 @@ pub const Context = struct {
         try loader.ref();
         errdefer loader.deref();
         self.vkb = try BaseDispatch.load(loader.getProcAddress);
+
         try self.initInstance(allocator);
+        errdefer self.vki.destroyInstance(self.instance, &self.vk_alloc);
+
+        try self.initDevice(allocator);
+        errdefer self.vkd.destroyDevice(self.device, &self.vk_alloc);
 
         return self;
     }
 
     pub fn deinit(self: Context) void {
+        self.vkd.destroyDevice(self.device, &self.vk_alloc);
         self.vki.destroyInstance(self.instance, &self.vk_alloc);
         loader.deref();
     }
@@ -39,7 +47,6 @@ pub const Context = struct {
         const app_version = std.meta.globalOption("zcompute_app_version", u32) orelse 0;
         const layers = try self.instanceLayers(allocator);
         defer allocator.free(layers);
-        const exts: [][*:0]const u8 = &.{};
 
         self.instance = try self.vkb.createInstance(.{
             .flags = .{},
@@ -52,11 +59,10 @@ pub const Context = struct {
             },
             .enabled_layer_count = @intCast(u32, layers.len),
             .pp_enabled_layer_names = layers.ptr,
-            .enabled_extension_count = @intCast(u32, exts.len),
-            .pp_enabled_extension_names = exts.ptr,
+            .enabled_extension_count = 0,
+            .pp_enabled_extension_names = undefined,
         }, &self.vk_alloc);
         self.vki = try InstanceDispatch.load(self.instance, self.vkb.dispatch.vkGetInstanceProcAddr);
-        errdefer self.vki.destroyInstance(self.instance, &self.vk_alloc);
     }
 
     fn instanceLayers(self: Context, allocator: *std.mem.Allocator) ![][*:0]const u8 {
@@ -77,7 +83,7 @@ pub const Context = struct {
         var n_layers: usize = 0;
         var layers: [wanted_layers.len][*:0]const u8 = undefined;
         for (wanted_layers) |wanted| {
-            for (supported_layers) |supported| {
+            for (supported_layers[0..n_supported_layers]) |supported| {
                 if (std.mem.eql(u8, wanted, std.mem.sliceTo(&supported.layer_name, 0))) {
                     layers[n_layers] = wanted.ptr;
                     n_layers += 1;
@@ -89,6 +95,61 @@ pub const Context = struct {
         }
 
         return allocator.dupe([*:0]const u8, layers[0..n_layers]);
+    }
+
+    fn initDevice(self: *Context, allocator: *std.mem.Allocator) !void {
+        // Find best physical device
+        var n_devices: u32 = undefined;
+        _ = try self.vki.enumeratePhysicalDevices(self.instance, &n_devices, null);
+        const devices = try allocator.alloc(vk.PhysicalDevice, n_devices);
+        defer allocator.free(devices);
+        _ = try self.vki.enumeratePhysicalDevices(self.instance, &n_devices, devices.ptr);
+
+        var compute_family: u32 = undefined;
+        self.phys_device = for (devices[0..n_devices]) |dev| {
+            var n_queues: u32 = undefined;
+            self.vki.getPhysicalDeviceQueueFamilyProperties(dev, &n_queues, null);
+            const queues = try allocator.alloc(vk.QueueFamilyProperties, n_queues);
+            defer allocator.free(queues);
+            self.vki.getPhysicalDeviceQueueFamilyProperties(dev, &n_queues, queues.ptr);
+
+            compute_family = for (queues[0..n_queues]) |queue, i| {
+                if (queue.queue_flags.compute_bit) {
+                    break @intCast(u32, i);
+                }
+            } else {
+                continue;
+            };
+
+            break dev;
+        } else {
+            return error.NoSuitableDevice;
+        };
+
+        // Create logical device
+        const queue_infos = [_]vk.DeviceQueueCreateInfo{
+            .{
+                .flags = .{},
+                .queue_family_index = compute_family,
+                .queue_count = 1,
+                .p_queue_priorities = &[1]f32{1.0},
+            },
+        };
+
+        self.device = try self.vki.createDevice(self.phys_device, .{
+            .flags = .{},
+            .queue_create_info_count = queue_infos.len,
+            .p_queue_create_infos = &queue_infos,
+            .enabled_layer_count = 0,
+            .pp_enabled_layer_names = undefined,
+            .enabled_extension_count = 0,
+            .pp_enabled_extension_names = undefined,
+            .p_enabled_features = null,
+        }, &self.vk_alloc);
+        self.vkd = try DeviceDispatch.load(self.device, self.vki.dispatch.vkGetDeviceProcAddr);
+        errdefer self.vkd.destroyDevice(self.device, &self.vk_alloc);
+
+        self.queue = self.vkd.getDeviceQueue(self.device, compute_family, 0);
     }
 };
 
