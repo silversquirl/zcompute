de733bf890d4287b6ca0afd01679094aa6285127
Silver <silver@silv.re>
2021-11-16 20:24:46 +0000

    Ensure examples are dynamically-linked on all targets

--- examples/build.zig
+++ examples/build.zig
@@ -9,6 +9,7 @@ pub fn build(b: *std.build.Builder) void {
 fn example(b: *std.build.Builder, target: std.build.Target, mode: std.builtin.Mode, name: []const u8) void {
     const exe = b.addExecutable(name, b.fmt("{s}/main.zig", .{name}));
     exe.addPackagePath("zcompute", "../src/zcompute.zig");
+    exe.linkage = .dynamic;
     exe.linkLibC();
     exe.setTarget(target);
     exe.setBuildMode(mode);
